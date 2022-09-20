fb5f2c55d6b9e5e547980003594e021b78890901
vaibhavlohiya <vaibhavlohiya05@gmail.com>
2022-09-21 05:05:04 +0530

    dot and cross operators to use

--- dot_cross_op/main.cpp
+++ dot_cross_op/main.cpp
@@ -1,5 +1,7 @@
 // Triple Product of 3 vectors ' a x (b x c) ' and 'a . (b x c)'
 
+// Make this into a single class with functions like dot product, cross product etc.
+
 #include <iostream>
 #include <vector>
 #include <array>
@@ -34,10 +36,10 @@ std::ostream& operator<<(std::ostream& stream, const Matrix& element)
     return stream;
 }
 
-float Dot_Product(Matrix& element0, Matrix& element1)
+/*float Dot_Product(Matrix& element0, Matrix& element1)
 {
     return (element0.X)*(element1.X) + (element0.Y)*(element1.Y) + (element0.Z)*(element1.Z);
-}
+}*/
 
 Matrix Cross_Product(Matrix& element0, Matrix& element1)
 {   
@@ -48,6 +50,21 @@ Matrix Cross_Product(Matrix& element0, Matrix& element1)
 
     return Result;
 }
+
+float operator*(const Matrix& element0,const Matrix& element1) // we are using '*' b/w two vectors for dot product. 
+{
+    return (element0.X)*(element1.X) + (element0.Y)*(element1.Y) + (element0.Z)*(element1.Z);
+}
+
+/*Matrix& operator*=(const Matrix& element0,const Matrix& element1) 
+{   
+    Matrix Result = {};
+    Result.X = (element0.Y*element1.Z) - (element0.Z*element1.Y);
+    Result.Y = (element0.Z*element1.X) - (element0.X*element1.Z);
+    Result.Z = (element0.X*element1.Y) - (element0.Y*element1.X);
+
+    return Result;
+}*/
 
 int main()
 {
@@ -66,16 +83,17 @@ int main()
     }
 
     // Dot Product
-    //std::cout << "Dot Product : " << Dot_Product(element.at(0), element.at(1)) << std::endl;
+    std::cout << "Dot Product : " << element.at(0)*element.at(1) << std::endl;
     
     // Cross Product
-    //std::cout << "Cross Product : " << Cross_Product(element.at(0), element.at(1)) << std::endl;
+    std::cout << "Cross Product : " << Cross_Product(element.at(0), element.at(1));
 
-    
     Matrix b_X_c = Cross_Product(element.at(1), element.at(2));
 
+    std::cout << "\n";
+
     // Scaler Triple Product 
-    std::cout << "Scaler Triple Product: " << Dot_Product(element.at(0), b_X_c);
+    std::cout << "Scaler Triple Product: " << element.at(0)*b_X_c;
 
     std::cout << "\n";
 
